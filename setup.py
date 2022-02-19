55f328d99e23c1ea21420e39a46322f0bf1bf13d
runeblaze <runeblaze@excite.co.jp>
2022-02-18 22:12:35 -0600

    fixed alpha5 typo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="magus-msa",
-    version="0.1.0a4",
+    version="0.1.0a6",
     author="vlasmirnov",
     description="Multiple Sequence Alignment using Graph Clustering",
     long_description=long_description,
