776f1095ece66d897dafcde39120fd80e8665f90
runeblaze <runeblaze@excite.co.jp>
2022-02-18 22:18:16 -0600

    added tools into installation

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 import setuptools
 from setuptools import find_packages
+import os
+
+# https://stackoverflow.com/a/36693250/13241395
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="magus-msa",
-    version="0.1.0a6",
+    version="0.1.0a7",
     author="vlasmirnov",
     description="Multiple Sequence Alignment using Graph Clustering",
     long_description=long_description,
@@ -26,6 +35,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
     ],
     package_dir={"": "."},
+    package_data={'': package_files('tools')},
     packages=find_packages(),
     install_requires=["dendropy>=4.5.2"],
     python_requires=">=3.6",
